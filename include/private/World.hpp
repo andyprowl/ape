e2cd659869e1d04d8e41e57b2147996fc3caee82
Proli,Andrea <andrea.proli@honeywell.com>
2019-09-15 00:58:20 +0200

    Extract Lighting class to separate header

--- include/private/World.hpp
+++ include/private/World.hpp
@@ -2,38 +2,9 @@
 
 #include "Body.hpp"
 #include "Camera.hpp"
-#include "DirectionalLight.hpp"
-#include "PointLight.hpp"
-#include "SpotLight.hpp"
+#include "Lighting.hpp"
 
 #include <vector>
-
-class Lighting
-{
-
-public:
-
-    Lighting() = default;
-
-    Lighting(
-        std::vector<PointLight> point,
-        std::vector<SpotLight> spot,
-        std::vector<DirectionalLight> directional)
-        : point{std::move(point)}
-        , spot{std::move(spot)}
-        , directional{std::move(directional)}
-    {
-    }
-
-public:
-
-    std::vector<PointLight> point;
-
-    std::vector<SpotLight> spot;
-
-    std::vector<DirectionalLight> directional;
-
-};
 
 class World
 {
