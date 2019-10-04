84f57ab8092263582e6da0bed1c8a5ba4130073a
Andy Prowl <andy.prowl@gmail.com>
2019-10-04 16:52:37 +0200

    Simplify geometry shader for wireframe rendering by eliminating round connectors - they were too slow and weren't rendered 100% correctly either

--- Ape/Engine/resources/Shaders/Wireframe/Wire.Geometry.glsl
+++ Ape/Engine/resources/Shaders/Wireframe/Wire.Geometry.glsl
@@ -1,35 +1,19 @@
 #version 430 core
 
-/*
-Vertex generation procedure:
- 
- - The input is an array of 3 vertices/sides. For each vertex/side:
-  - Emit a thick line (produces 4 vertices)
-  - Emit a round connector made of 2 triangle fans. For each fan:
-   - Emit "numOfConnectorTriangles" triangles, each with 3 vertices.
-
-Number of generated vertices:
-
- => 3 x (4 + 2 * numOfConnectorTriangles * 3) = 12 + 18 * numOfConnectorTriangles
-
-Examples:
-
- 16 triangles per connector => 12 + 18 * 16 = 300 vertices
- 10 triangles per connector => 12 + 18 * 10 = 192 vertices
-  8 triangles per connector => 12 + 18 * 8  = 156 vertices
-*/
-
 layout(triangles) in;
 
-layout(triangle_strip, max_vertices = 192) out;
+layout(triangle_strip, max_vertices = 16) out;
 
-uniform float lineWidth = 0.05;
-
-const int numOfConnectorTriangles = 10;
+uniform float lineWidth = 0.03;
 
 vec4 getClipSpaceVertex(int i)
 {
     return gl_in[i].gl_Position;
+}
+
+vec4[3] getClipSpaceVertices()
+{
+    return vec4[3](getClipSpaceVertex(0), getClipSpaceVertex(1), getClipSpaceVertex(2));
 }
 
 vec2 getClipSpaceNormal(vec4 source, vec4 target)
@@ -43,17 +27,12 @@ vec2 getClipSpaceNormal(vec4 source, vec4 target)
     return normalize(vec2(edge.y, -edge.x));
 }
 
-vec4[3] getClipSpaceVertices()
-{
-    return vec4[3](getClipSpaceVertex(0), getClipSpaceVertex(1), getClipSpaceVertex(2));
-}
-
-vec2[3] getClipSpaceNormals(vec4 vertices[3])
+vec2[3] getClipSpaceNormals(vec4 clipSpaceVertices[3])
 {
     return vec2[3](
-        getClipSpaceNormal(vertices[0], vertices[1]),
-        getClipSpaceNormal(vertices[1], vertices[2]),
-        getClipSpaceNormal(vertices[2], vertices[0]));
+        getClipSpaceNormal(clipSpaceVertices[0], clipSpaceVertices[1]),
+        getClipSpaceNormal(clipSpaceVertices[1], clipSpaceVertices[2]),
+        getClipSpaceNormal(clipSpaceVertices[2], clipSpaceVertices[0]));
 }
 
 void emitVertex(vec4 position)
@@ -65,45 +44,15 @@ void emitVertex(vec4 position)
 
 void emitThickLine(vec4 source, vec4 target, vec2 normal)
 {
-    vec2 offset = normal * lineWidth;
+    vec4 offset = vec4(normal * lineWidth, 0.0, 0.0);
 
-    emitVertex(source - vec4(offset, 0.0, 0.0));
+    emitVertex(source - offset);
 
-    emitVertex(source + vec4(offset, 0.0, 0.0));
+    emitVertex(source + offset);
 
-    emitVertex(target - vec4(offset, 0.0, 0.0));
+    emitVertex(target - offset);
 
-    emitVertex(target + vec4(offset, 0.0, 0.0));
-}
-
-vec2 getTriangleFanVertex(vec2 source, vec2 target, float interpolator, float amplitude)
-{
-    return normalize(mix(source, target, interpolator)) * amplitude;
-}
-
-void emitTriangleFan(vec4 center, vec2 source, vec2 target, float amplitude, int numOfTriangles)
-{
-    float interpolationStep = (1.0 / numOfTriangles);
-
-    for (int i = 0; i < numOfTriangles; ++i)
-    {
-        emitVertex(center);
-
-        vec2 v1 = getTriangleFanVertex(source, target, interpolationStep * i, amplitude);
-
-        emitVertex(center + vec4(v1, 0.0, 0.0));
-
-        vec2 v2 = getTriangleFanVertex(source, target, interpolationStep * (i + 1), amplitude);
-
-        emitVertex(center + vec4(v2, 0.0, 0.0));
-    }
-}
-
-void emitRoundConnector(vec4 vertex, vec2 sourceNormal, vec2 targetNormal)
-{
-    emitTriangleFan(vertex, sourceNormal, targetNormal, lineWidth, numOfConnectorTriangles);
-
-    emitTriangleFan(vertex, -targetNormal, -sourceNormal, lineWidth, numOfConnectorTriangles);
+    emitVertex(target + offset);
 }
 
 void main()
@@ -112,20 +61,14 @@ void main()
 
     vec2 normals[] = getClipSpaceNormals(vertices);
 
-    for (int i = 0; i < 3; ++i)
+    for (int i = 0; i < 4; ++i)
     {
-        vec4 thisPosition = vertices[i];
+        vec4 thisVertex = vertices[i % 3];
 
-        vec4 nextPosition = vertices[(i + 1) % 3];
+        vec4 nextVertex = vertices[(i + 1) % 3];
 
-        vec2 thisNormal = normals[i];
+        vec2 thisNormal = normals[i % 3];
 
-        vec2 nextNormal = normals[(i + 1) % 3];
-
-        emitThickLine(thisPosition, nextPosition, thisNormal);
-    
-        emitRoundConnector(nextPosition, thisNormal, nextNormal);
+        emitThickLine(thisVertex, nextVertex, thisNormal);
     }
-
-    EndPrimitive();
 }
